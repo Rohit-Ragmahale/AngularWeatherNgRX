4abe0c8bf8a2b0322506fa81ab1178a7dc6843cf
Rohit Ragmahale <rohitragmahale.mobile@gmail.com>
2020-04-29 04:03:25 +0530

    add reactive form control for accepting zip code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WeatherNgRx';
+
+  userForm: FormGroup;
+ 
+  newZip = ""; 
+
+   constructor(private builder: FormBuilder) {
+
+    this.userForm = this.builder.group({
+      zip: this.builder.control('', [Validators.required, 
+                                    Validators.minLength(6),
+                                    Validators.maxLength(6),
+                                    Validators.pattern("[0-9]{6}")  
+                                  ])
+    });
+  }
+
+  addZipCode() {
+    this.newZip = this.userForm.controls.zip.value;
+  }
 }
