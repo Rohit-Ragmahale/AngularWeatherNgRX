82d04195a76c48570fcc5298af8da3ef235743ff
Rohit Ragmahale <rohitragmahale.mobile@gmail.com>
2020-05-01 19:44:19 +0530

    removed spec files

--- src/app/weather-item.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WeatherItemDirective } from './weather-item.directive';
-
-describe('WeatherItemDirective', () => {
-  it('should create an instance', () => {
-    const directive = new WeatherItemDirective();
-    expect(directive).toBeTruthy();
-  });
-});
